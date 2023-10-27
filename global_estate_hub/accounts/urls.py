d5883a10c3f0d500c1323b82220980d4c4003c8d
Hubert Rykała <hubert.rykala@gmail.com>
2023-10-28 00:32:45 +0200

    log-in.html created

--- global_estate_hub/accounts/urls.py
+++ global_estate_hub/accounts/urls.py
@@ -3,4 +3,5 @@ from . import views as accounts_views
 
 urlpatterns = [
     path(route='sign-up/', view=accounts_views.sign_up, name='sign-up'),
+    path(route='log-in/', view=accounts_views.log_in, name='log-in')
 ]
--- global_estate_hub/accounts/views.py
+++ global_estate_hub/accounts/views.py
@@ -5,3 +5,9 @@ def sign_up(request):
     return render(request=request, template_name='accounts/sign-up.html', context={
         'title': 'Sign Up',
     })
+
+
+def log_in(request):
+    return render(request=request, template_name='accounts/log-in.html', context={
+        'title': 'Login'
+    })
