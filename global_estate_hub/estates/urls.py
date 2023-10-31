fd4ff4591326e5902ee378c3353868dcb17f0846
Hubert Rykała <hubert.rykala@gmail.com>
2023-10-31 21:48:57 +0100

    update

--- global_estate_hub/estates/urls.py
+++ global_estate_hub/estates/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path(route='about', view=estates_views.about, name='about'),
     path(route='properties', view=estates_views.properties, name='properties'),
     path(route='pages', view=estates_views.properties, name='pages'),
-    path(route='blogs', view=estates_views.properties, name='blogs'),
-    path(route='contact', view=estates_views.properties, name='contact'),
+    path(route='blogs', view=estates_views.blogs, name='blogs'),
+    path(route='contact', view=estates_views.contact, name='contact'),
     path(route='add-category', view=estates_views.add_category, name='add-category'),
 ]
--- global_estate_hub/estates/views.py
+++ global_estate_hub/estates/views.py
@@ -8,6 +8,29 @@ def index(request):
 
 
 def about(request):
+    # print(f"Path -> '{request.path}'")
+    # print(f"Scheme -> '{request.scheme}'")
+    # print(f"Body -> '{request.body}'")
+    # print(f"Path Info -> '{request.path_info}'")
+    # print(f"Method -> '{request.method}'")
+    # print(f"Encoding -> '{request.encoding}'")
+    # print(f"Content Type -> '{request.content_type}'")
+    # print(f"Content Params -> '{request.content_params}'")
+    # print(f"GET -> '{request.GET}'")
+    # print(f"POST -> '{request.POST}'")
+    # print(f"Cookies -> '{request.COOKIES}'")
+    # print(f"Files -> '{request.FILES}'")
+    # print(f"Meta -> '{request.META}'")
+    # print(f"Header -> '{request.headers['Accept-Encoding']}'")
+    # print(f"Session -> '{request.session.__dict__}'")
+    # print(f"User -> '{request.user}'")
+    # print(f"Get Host -> '{request.get_host()}'")
+    # print(f"Get Port -> '{request.get_port()}'")
+    # print(f"Build Absolute Uri -> '{request.build_absolute_uri()}'")
+    # print(f"Is Secure -> '{request.is_secure()}'")
+    # print(f"Accepts -> '{request.accepts(media_type=request.content_type)}'")
+    # print(request.user.is_authenticated)
+
     return render(request=request, template_name='estates/about.html', context={
         'title': 'About',
     })
