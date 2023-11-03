6953bd8e29707f75da596b83f83e076de875975c
hubertrykala93 <hubert.rykala@gmail.com>
2023-11-03 19:41:04 +0100

    request body

--- global_estate_hub/estates/views.py
+++ global_estate_hub/estates/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Newsletter
-from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 def index(request):
@@ -10,12 +10,16 @@ def index(request):
     })
 
 
-@csrf_exempt
 def newsletter(request):
     if request.method == 'POST':
-        email = request.POST.get('email')
-        print(request.POST)
-        print(request.POST.get('email'))
+        email = json.loads(s=request.body.decode('utf-8'))['email']
+        print(f"Request Body -> {request.body}")
+        print()
+        print(f"Request.POST -> {request.POST}")
+        print()
+        print(f"Request Body Decoded -> {request.body.decode('utf-8')}")
+        print()
+        print(email)
 
         if email is not None and len(email) == 0:
             return JsonResponse(data={
@@ -49,6 +53,45 @@ def newsletter(request):
             "valid": False,
             "message": "The email address was not added.",
         })
+
+
+# @csrf_exempt
+# def newsletter(request):
+#     if request.method == 'POST':
+#         email = request.POST.get('email')
+#
+#         if email is not None and len(email) == 0:
+#             return JsonResponse(data={
+#                 "valid": False,
+#                 "message": "The email address cannot be empty.",
+#             })
+#
+#         elif Newsletter.objects.filter(email=email).exists():
+#             return JsonResponse(data={
+#                 "valid": False,
+#                 "message": "The email address already exists.",
+#             })
+#
+#         elif email is None:
+#             return JsonResponse(data={
+#                 "valid": False,
+#                 "message": "The email address is None.",
+#             })
+#
+#         else:
+#             new_subscriber = Newsletter(email=email)
+#             new_subscriber.save()
+#
+#             return JsonResponse(data={
+#                 "valid": True,
+#                 "message": "Congratulations! You have subscribed to our newsletter.",
+#             })
+#
+#     else:
+#         return JsonResponse(data={
+#             "valid": False,
+#             "message": "The email address was not added.",
+#         })
 
 
 def about(request):
