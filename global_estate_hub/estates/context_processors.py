a8bbacf74f47370e67571a5dc08a496942f8e5d3
hubertrykala93 <hubert.rykala@gmail.com>
2023-11-03 19:05:23 +0100

    generate csrf token

--- global_estate_hub/estates/context_processors.py
+++ global_estate_hub/estates/context_processors.py
@@ -1,4 +1,13 @@
+from django.middleware.csrf import get_token
+
+
 def breadcrumbs_urls(request):
     return {
         'breadcrumbs_urls': request.path.split('/')[1:],
+    }
+
+
+def generate_token(request):
+    return {
+        'csrf_token': get_token(request=request)
     }
