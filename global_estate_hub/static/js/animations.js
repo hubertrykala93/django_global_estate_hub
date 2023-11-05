fb1d5dbb38802f614c4ee039184cb3fecf5503ce
Szymon-Levy <94991990+Szymon-Levy@users.noreply.github.com>
2023-11-05 22:35:31 +0100

    about done

--- global_estate_hub/static/js/animations.js
+++ global_estate_hub/static/js/animations.js
@@ -2,7 +2,7 @@
    * PAGE TITLE
     */
 
-const $pageTitle = document.querySelector('.page-title')
+const $pageTitle = document.querySelector('[data-pagetitle-animation]')
 
 if ( $pageTitle ){
 
@@ -23,6 +23,100 @@ const breadcrumbsListItems = {
   interval: 150,
 }
 
-ScrollReveal().reveal('.page-title .title', titleAnimation)
-ScrollReveal().reveal('.page-title .breadcrumbs li', breadcrumbsListItems)
+ScrollReveal().reveal($pageTitle.querySelector('.title'), titleAnimation)
+ScrollReveal().reveal($pageTitle.querySelectorAll('.breadcrumbs li'), breadcrumbsListItems)
+}
+
+
+/**
+   * HOME PAGE
+    */
+
+const $aboutSection = document.querySelector('[data-aboutsection-animation]')
+
+if($aboutSection){
+
+  const singleImageAnimation = {
+    delay: 200,
+    distance: '30px',
+    duration: 900,
+    opacity: 0,
+    origin: 'left',
+    easing: 'ease'
+  }
+
+  const doubleImageTopAnimation = {
+    delay: 250,
+    distance: '30px',
+    duration: 900,
+    opacity: 0,
+    origin: 'top',
+    easing: 'ease'
+  }
+
+  const doubleImageBottomAnimation = {
+    delay: 300,
+    distance: '30px',
+    duration: 900,
+    opacity: 0,
+    origin: 'bottom',
+    easing: 'ease'
+  }
+
+  const listAnimation = {
+    delay: 100,
+    distance: '50px',
+    duration: 900,
+    opacity: 0,
+    origin: 'right',
+    easing: 'ease',
+    interval: 150,
+  }
+
+  ScrollReveal().reveal($aboutSection.querySelector('.image.single'), singleImageAnimation)
+  ScrollReveal().reveal($aboutSection.querySelector('.image.top'), doubleImageTopAnimation)
+  ScrollReveal().reveal($aboutSection.querySelector('.image.bottom'), doubleImageBottomAnimation)
+  ScrollReveal().reveal($aboutSection.querySelectorAll('.about__list .about__list-item'), listAnimation)
+}
+
+
+/**
+   * -SECTION TITLE
+    */
+
+const $sectionTitles = document.querySelectorAll('[data-sectiontitle-animation]')
+
+if ($sectionTitles.length){
+  const upperTitleAnimation = {
+    delay: 250,
+    distance: '30px',
+    duration: 700,
+    opacity: 0,
+    origin: 'bottom',
+    easing: 'ease'
+  }
+
+  const mainTitleAnimation = {
+    delay: 350,
+    distance: '30px',
+    duration: 700,
+    opacity: 0,
+    origin: 'bottom',
+    easing: 'ease'
+  }
+
+  $sectionTitles.forEach(item => {
+    const upperTitle = item.querySelector('.upper-title')
+    const mainTitle = item.querySelector('.main-title')
+    console.log(upperTitle)
+    console.log(mainTitle)
+
+    if(upperTitle){
+      ScrollReveal().reveal(upperTitle, upperTitleAnimation)
+    }
+    if(mainTitle){
+      ScrollReveal().reveal(mainTitle, mainTitleAnimation)
+    }
+  });
+
 }
