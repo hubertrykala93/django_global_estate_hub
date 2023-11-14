02aafacbcc58023b4c5cea41f683165daedcfb11
hubertrykala93 <hubert.rykala@gmail.com>
2023-11-14 21:04:24 +0100

    footer update

--- global_estate_hub/blog/views.py
+++ global_estate_hub/blog/views.py
@@ -7,20 +7,56 @@ def blog(request):
     paginator = Paginator(object_list=Article.objects.all(), per_page=3)
     page = request.GET.get('page')
 
-    try:
-        articles = paginator.get_page(number=page)
+    if request.resolver_match.url_name == 'blog':
+        if page is None:
+            try:
+                pages = paginator.get_page(number=page)
 
-    except PageNotAnInteger:
-        page = 1
-        articles = paginator.page(number=page)
+            except PageNotAnInteger:
+                page = 1
+                pages = paginator.page(number=page)
 
-    except EmptyPage:
-        page = paginator.num_pages
-        articles = paginator.page(number=page)
+            except EmptyPage:
+                page = paginator.num_pages
+                pages = paginator.page(number=page)
+
+        elif int(page) == 0:
+            try:
+                pages = paginator.get_page(number=1)
+
+            except PageNotAnInteger:
+                page = 1
+                pages = paginator.page(number=page)
+
+            except EmptyPage:
+                page = paginator.num_pages
+                pages = paginator.page(number=page)
+
+        elif paginator.num_pages >= int(page) >= 1:
+            try:
+                pages = paginator.get_page(number=page)
+
+            except PageNotAnInteger:
+                page = 1
+                pages = paginator.page(number=page)
+
+            except EmptyPage:
+                page = paginator.num_pages
+                pages = paginator.page(number=page)
+
+        else:
+            return render(request=request, template_name='core/error.html', context={
+                'title': 'Error 404',
+            })
+
+    else:
+        return render(request=request, template_name='core/error.html', context={
+            'title': 'Error 404',
+        })
 
     return render(request=request, template_name='blog/blog.html', context={
         'title': 'Blog',
-        'pages': articles,
+        'pages': pages,
         'paginator': paginator,
     })
 
