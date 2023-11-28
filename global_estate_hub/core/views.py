0ccf45ca89e1801285a35c1303b137531dd4db52
hubertrykala93 <hubert.rykala@gmail.com>
2023-11-28 21:01:25 +0100

    update core views

--- global_estate_hub/core/views.py
+++ global_estate_hub/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
 from .models import Newsletter, ContactMail
 import json
 import re
