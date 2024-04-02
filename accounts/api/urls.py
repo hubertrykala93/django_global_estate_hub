af787256ec3bf81f645207d18d80db8f44fa1e9d
hubertrykala93 <hubert.rykala@gmail.com>
2024-04-02 09:41:20 +0200

    working on api crud on accounts app

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route='api/users', view=views.UserAPIView.as_view(), name='api-users'),
-    path(route='api/individual-profiles', view=views.IndividualProfileAPIView.as_view(),
-         name='api-individual-profiles'),
-    path(route='api/business-profiles', view=views.BusinessProfileAPIView.as_view(), name='api-business-profiles'),
+    path(route='api', view=views.api_endpoints, name='api'),
+    path(route='api/v1/users', view=views.UserAPIView.as_view(), name='api-users'),
+    path(route='api/v1/users/<int:pk>', view=views.UserDetailsAPIView.as_view(), name='api-users-user-details'),
+    path(route='api/v1/users/create', view=views.UserCreateAPIView.as_view(), name='api-users-user-create'),
+    path(route='api/v1/users/<int:pk>/update', view=views.UserUpdateAPIView.as_view(), name='api-users-user-update'),
+    path(route='api/v1/users/<int:pk>/delete', view=views.UserDeleteAPIView.as_view(), name='api-users-user-delete'),
+    path(route='api/v1/users/individuals', view=views.IndividualProfileAPIView.as_view(),
+         name='api-users-individuals'),
+    path(route='api/v1/users/business', view=views.BusinessProfileAPIView.as_view(), name='api-users-business'),
 ]
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,6 +1,34 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, RetrieveUpdateAPIView, \
+    RetrieveDestroyAPIView
 from accounts.models import User, Individual, Business
 from .serializers import UserSerializer, IndividualProfileSerializer, BusinessProfileSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+
+@api_view(http_method_names=['GET'])
+def api_endpoints(request):
+    return Response(
+        data={
+            "Users": "api/v1/users",
+            "Search User by Is Agent": "api/v1/users/?is-agent=is_agent",
+            "Search User by Date Joined": "api/v1/users/?date-joined=date_joined",
+            "User Details": "api/v1/users/pk",
+            "User Create": "api/v1/users/create",
+            "User Update": "api/v1/users/pk/update",
+            "User Delete": "api/v1/users/pk/delete",
+            "User Profiles": "api/v1/users/profiles",
+            "Search User Profile by Gender": "api/v1/users/profiles/?gender=gender",
+            "Search User Profile by Country": "api/v1/users/profiles/?country=country",
+            "Search User Profile by Province": "api/v1/users/profiles/?province=province",
+            "Search User Profile by City": "api/v1/users/profiles/?city=city",
+            "User Profile Details": "api/v1/users/?account-type=account_type/pk",
+            "User Profile Create": "api/v1/users/profiles/create",
+            "User Profile Update": "api/v1/users/?account-type=account_type/pk/update",
+            "User Profile Delete": "api/v1/users/?account-type=account_type/pk/delete",
+        },
+        status=status.HTTP_200_OK)
 
 
 class UserAPIView(ListAPIView):
@@ -12,6 +40,55 @@ class UserAPIView(ListAPIView):
 
     def get_view_description(self, html=False):
         return 'API view with all registered users on the Global Estate Hub platform.'
+
+
+class UserDetailsAPIView(RetrieveAPIView):
+    queryset = User.objects.all()
+    lookup_field = 'pk'
+    serializer_class = UserSerializer
+
+    def get_view_name(self):
+        return 'Global Estate Hub User Details'
+
+    def get_view_description(self, html=False):
+        return 'API view with details of the selected user on the Global Estate Hub platform.'
+
+
+class UserCreateAPIView(CreateAPIView):
+    # queryset = User.objects.all()
+    # serializer_class = UserCreateSerializer
+    #
+    # def get_view_name(self):
+    #     return 'Global Estate Hub User Create'
+    #
+    # def get_view_description(self, html=False):
+    #     return 'API view with the ability to create a new user on the Global Estate Hub platform.'
+    pass
+
+
+class UserUpdateAPIView(RetrieveUpdateAPIView):
+    # queryset = User.objects.all()
+    # serializer_class = UserUpdateSerializer
+    # lookup_field = 'pk'
+    #
+    # def get_view_name(self):
+    #     return 'Global Estate Hub User Update'
+    #
+    # def get_view_description(self, html=False):
+    #     return 'API view with the ability to update an existing user on the Global Estate Hub platform.'
+    pass
+
+
+class UserDeleteAPIView(RetrieveDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'pk'
+
+    def get_view_name(self):
+        return 'Global Estate Hub User Delete'
+
+    def get_view_description(self, html=False):
+        return 'API view with the ability do delete an existing user on the Global Estate Hub platform.'
 
 
 class IndividualProfileAPIView(ListAPIView):
